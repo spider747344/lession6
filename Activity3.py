4add5e3f32597909c8d27daba3d0b507f61afdd2
spider747344 <9630003948sri@gmail.com>
2025-03-25 20:54:38 +0530

    Activity3.py

---
+++ Activity3.py
@@ -0,0 +1,20 @@
+height = float(input("Enter your height in cm: "))
+weight = float(input("enter your weight in kg: "))
+
+BMI = weight / (height/100)**2
+
+print("Your BMI is",BMI)
+
+if BMI <=18.4:
+   print("You are underweight")
+elif BMI <=24.9:
+   print("you are healthy")
+elif BMI <=29.9:
+   print("you are over weight")
+elif BMI <=34.9:
+   print("you are severely over weight")
+elif BMI <=39.9:
+    print("you are obese")
+
+else:
+   print("You are severely obese")
